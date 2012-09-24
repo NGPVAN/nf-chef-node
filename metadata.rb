196d93d65778fd39ef8f194a96f514a04e6e48bf
Graham Christensen <gchristensen@nationalfield.org>
2012-09-24 15:09:31 -0400

    Manually add the ppa instead of using a broken ppa helper.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ description      "Installs/Configures node and NPM via apt"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-depends          "apt-repo" #see: https://github.com/sometimesfood/chef-apt-repo
+depends          "apt"
 supports         "ubuntu-12.04"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,13 @@
 # Copyright 2012, NationalField, LLC
 #
 
-ppa "ppa:chris-lea/node.js"
+apt_repository "chris-lea-node-js" do
+    uri "http://ppa.launchpad.net/chris-lea/node.js/ubuntu/"
+    distribution node["lsb"]["codename"]
+    components ["main"]
+    keyserver "keyserver.ubuntu.com"
+    key "C7917B12"
+end
 
 package "nodejs" do
     version "0.8.9-4chl1~precise1"
